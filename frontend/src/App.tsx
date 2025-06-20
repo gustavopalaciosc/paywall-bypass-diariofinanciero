cd2a0f6858e0499a488e715be57b3a0c7d6058c1
gustavopalaciosc <gpalaciosc@uc.cl>
2025-06-20 16:41:41 -0400

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <h1>Hola que tal</h1>
+    </div>
+
+    </>
+  )
+}
+
+export default App
