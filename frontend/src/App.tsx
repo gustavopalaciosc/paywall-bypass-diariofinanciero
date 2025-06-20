da05be8c6cec51d194ecd5283333fc0e244e133e
gustavopalaciosc <gpalaciosc@uc.cl>
2025-06-20 17:11:42 -0400

    Setup tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-    <div>
-      <h1>Hola que tal</h1>
-    </div>
+ 
 
     </>
   )
