eedf6a82225cb842e3d57737f883596993c1cdd1
gustavopalaciosc <gpalaciosc@uc.cl>
2025-06-22 12:49:25 -0400

    style: minor change in components

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -5,7 +5,7 @@ const Button = () => {
 
     return (
         <button
-            className="px-8 py-2 border rounded-md font-medium
+            className="px-8 py-2 border rounded-md font-medium text-sm md:text-base 
             bg-gradient-to-r from-orange-400 via-orange-500 to-orange-500"
         >
             Extraer
--- frontend/src/components/CopyButton.tsx
+++ frontend/src/components/CopyButton.tsx
@@ -4,7 +4,7 @@ import { LuCopy } from "react-icons/lu";
 
 const CopyButton = () => {
     return (
-        <button className="px-2 py-1 flex items-center space-x-3 rounded-md border border-orange-300 bg-white">
+        <button className="px-2 py-1 flex items-center text-xs space-x-3 rounded-md border border-orange-300 bg-white">
             <LuCopy />
             <span>Copiar</span>
         </button>
